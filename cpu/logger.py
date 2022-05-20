2e9807db130f68cee14b4e1429e1694456d624d0
serend1p1ty <zjli1997@163.com>
2022-05-21 02:34:40 +0800

    optimize logger.py

--- cpu/logger.py
+++ cpu/logger.py
@@ -8,13 +8,22 @@ from termcolor import colored
 logger_initialized = {}
 
 
-def setup_logger(
-    name: Optional[str] = None,
-    output_dir: Optional[str] = None,
-    rank: int = 0,
-    log_level: int = logging.DEBUG,
-    color: bool = True,
-) -> logging.Logger:
+class _ColorfulFormatter(logging.Formatter):
+    def formatMessage(self, record):
+        log = super(_ColorfulFormatter, self).formatMessage(record)
+        if record.levelno == logging.DEBUG:
+            prefix = colored("DEBUG", "magenta")
+        elif record.levelno == logging.WARNING:
+            prefix = colored("WARNING", "red", attrs=["blink"])
+        elif record.levelno == logging.ERROR or record.levelno == logging.CRITICAL:
+            prefix = colored("ERROR", "red", attrs=["blink", "underline"])
+        else:
+            return log
+        return prefix + " " + log
+
+
+def setup_logger(name: Optional[str] = None, output_dir: Optional[str] = None, rank: int = 0,
+                 log_level: int = logging.DEBUG, color: bool = True) -> logging.Logger:
     """Initialize the logger.
 
     If the logger has not been initialized, this method will initialize the
@@ -41,23 +50,24 @@ def setup_logger(
     # the messages of this logger will not be propagated to its parent
     logger.propagate = False
 
-    fmt = "[%(asctime)s %(name)s] (%(filename)s %(lineno)d): %(levelname)s %(message)s"
-    color_fmt = colored("[%(asctime)s %(name)s]", "green") + \
-                colored("(%(filename)s %(lineno)d)", "yellow") + ": %(levelname)s %(message)s"
+    formatter = logging.Formatter(
+        "[%(asctime)s %(name)s %(levelname)s]: %(message)s",
+        datefmt="%m/%d %H:%M:%S")
+    color_formatter = _ColorfulFormatter(
+        colored("[%(asctime)s %(name)s]: ", "green") + "%(message)s",
+        datefmt="%m/%d %H:%M:%S")
 
     # create console handler for master process
     if rank == 0:
         console_handler = logging.StreamHandler(stream=sys.stdout)
         console_handler.setLevel(log_level)
-        formatter = logging.Formatter(fmt=color_fmt if color else fmt, datefmt="%Y-%m-%d %H:%M:%S")
-        console_handler.setFormatter(formatter)
+        console_handler.setFormatter(color_formatter if color else formatter)
         logger.addHandler(console_handler)
 
     if output_dir is not None:
         os.makedirs(output_dir, exist_ok=True)
         file_handler = logging.FileHandler(os.path.join(output_dir, f"log_rank{rank}.txt"))
         file_handler.setLevel(log_level)
-        formatter = logging.Formatter(fmt=fmt, datefmt="%Y-%m-%d %H:%M:%S")
         file_handler.setFormatter(formatter)
         logger.addHandler(file_handler)
 
