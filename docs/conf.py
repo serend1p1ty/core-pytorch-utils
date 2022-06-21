5359ce6a7cbacb86a0bb8ba376ea51419502881c
serend1p1ty <zjli1997@163.com>
2022-06-21 18:16:40 +0800

    use cpu.__version__ as docs version

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,9 @@ copyright = "2022, serend1p1ty"
 author = "serend1p1ty"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+import cpu  # noqa: E402
+
+release = cpu.__version__
 
 # -- General configuration ---------------------------------------------------
 
