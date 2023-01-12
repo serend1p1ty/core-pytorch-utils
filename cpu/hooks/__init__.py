7e8eb1079b131c16e28249a5ca4ce979533bc357
wenju.li <wenju.li@deepctr.cn>
2023-01-12 11:17:07 +0800

    添加iter版本的checkpoint hook 和eval hook

--- cpu/hooks/__init__.py
+++ cpu/hooks/__init__.py
@@ -1,6 +1,6 @@
-from .checkpoint_hook import CheckpointHook
+from .checkpoint_hook import CheckpointHook, IterCheckpointHook
 from .distributed_hook import DistributedHook
-from .eval_hook import EvalHook
+from .eval_hook import EvalHook, IterEvalHook
 from .hookbase import HookBase
 from .logger_hook import LoggerHook
 from .lr_update_hook import LRUpdateHook
--- cpu/hooks/checkpoint_hook.py
+++ cpu/hooks/checkpoint_hook.py
@@ -43,3 +43,43 @@ class CheckpointHook(HookBase):
 
     def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
         self.__dict__.update(state_dict)
+
+
+class IterCheckpointHook(HookBase):
+    """Save checkpoint periodically.
+
+    Save checkpoint, if current steps is a multiple of ``period`` or ``max_iter`` is reached.
+
+    Args:
+        period (int): Save checkpoint every ``period`` iters.
+        max_to_keep (int): Maximum number of most current checkpoints to keep,
+            previous checkpoints will be deleted. If None, save all checkpoints.
+    """
+
+    def __init__(self, period: int, max_to_keep: Optional[int] = None) -> None:
+        self._period = period
+        assert max_to_keep is None or max_to_keep > 0
+        self._max_to_keep = max_to_keep
+
+        self._recent_checkpoints: List[str] = []
+
+    def after_iter(self) -> None:
+        if self.every_n_iters(self._period) or self.is_last_iter():
+            iter = self.trainer.cur_iter  # ranged in [0, max_iters - 1]
+            checkpoint_name = f"iter_{iter}.pth"
+            self.trainer.save_checkpoint(checkpoint_name)
+
+            if self._max_to_keep is not None:
+                self._recent_checkpoints.append(checkpoint_name)
+                if len(self._recent_checkpoints) > self._max_to_keep:
+                    # delete the oldest checkpoint
+                    file_name = self._recent_checkpoints.pop(0)
+                    file_path = osp.join(self.trainer.ckpt_dir, file_name)
+                    if os.path.exists(file_path):
+                        os.remove(file_path)
+
+    def state_dict(self) -> Dict[str, Any]:
+        return {key: value for key, value in self.__dict__.items() if key != "trainer"}
+
+    def load_state_dict(self, state_dict: Dict[str, Any]) -> None:
+        self.__dict__.update(state_dict)
