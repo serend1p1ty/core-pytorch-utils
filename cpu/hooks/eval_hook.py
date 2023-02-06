4683684cb22b395c1510e570f8f72da6a6247b53
wenju.li <wenju.li@deepctr.cn>
2023-02-06 13:25:54 +0800

    add iter checkpoint hook and iter eval hook

--- cpu/hooks/eval_hook.py
+++ cpu/hooks/eval_hook.py
@@ -38,3 +38,42 @@ class EvalHook(HookBase):
     def after_epoch(self) -> None:
         if self.every_n_epochs(self._period) or self.is_last_epoch():
             self._do_eval()
+
+
+
+class IterEvalHook(HookBase):
+    """Run an evaluation function periodically.
+
+    It is executed every ``period`` steps and after the last epoch.
+
+    Args:
+        period (int): The period to run ``eval_func``. Set to 0 to
+            not evaluate periodically, but still after the last epoch.
+        eval_func (callable): A function which takes no arguments, and
+            returns a dict of evaluation metrics.
+    """
+
+    priority = 1
+
+    def __init__(self, period: int, eval_func: Callable) -> None:
+        self._period = period
+        self._eval_func = eval_func
+
+    def _do_eval(self) -> None:
+        res = self._eval_func()
+
+        if res:
+            assert isinstance(res, dict), f"Eval function must return a dict. Got {res} instead."
+            for k, v in res.items():
+                try:
+                    v = float(v)
+                except Exception as e:
+                    raise ValueError(
+                        f"Eval function should return a dict of float. Got '{k}: {v}' instead."
+                    ) from e
+            self.log(self.trainer.epoch, **res, smooth=False)
+
+
+    def after_iter(self) -> None:
+        if self.every_n_iters(self._period) or self.is_last_iter():
+            self._do_eval()
